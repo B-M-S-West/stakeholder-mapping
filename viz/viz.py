9e4a75b6d5a74dc2984cba5a1174581e137df47e
Ben West <b-m-s-west@outlook.com>
2025-09-19 18:08:33 +0100

    Update - Build graph, organisations built

--- viz/viz.py
+++ viz/viz.py
@@ -9,3 +9,23 @@ DB_PATH = "../govmap_db"
 db = kuzu.Database(DB_PATH)
 conn = kuzu.Connection(db)
 
+def build_graph():
+    nodes, edges, org_edges = [], [], []
+
+    # Organisations
+    orgs = conn.execute("""
+        MATCH (o:Organisation)
+        RETURN o.org_id, o.org_name, o.org_type, o.org_function
+    """).get_as_python()
+    for oid, name, otype, ofunc in orgs:
+        nodes.append({
+            "data": {
+                "id": f"org{oid}",
+                "label": name,
+                "role": "org",
+                "otype": otype,
+                "function": ofunc
+            }
+        })
+
+    
